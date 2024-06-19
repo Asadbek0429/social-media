a09f5cb6963a8739794086968d47df190015b5ae
Asadbek <ravshanbekovasadbek0429@gmail.com>
2024-06-19 13:57:39 +0500

    init

--- blog/models.py
+++ blog/models.py
@@ -14,36 +14,7 @@ class Post(models.Model):
     def __str__(self):
         return str(self.id)
 
-    @classmethod
-    def all_(cls):
-        return cls.objects.raw("SELECT * FROM blog_post")
-
-    @classmethod
-    def get_(cls, pk):
-        query = f"SELECT * FROM blog_post WHERE id={pk}"
-        return cls.objects.raw(query)[0]
-
-    @classmethod
-    def create_(cls, **data):
-        query = f"INSERT INTO blog_post (image, author) VALUES ({data['image']}, {data['author']})"
-        return cls.objects.raw(query)
-
-    @classmethod
-    def delete_(cls, pk):
-        query = f"DELETE FROM blog_post WHERE id={pk}"
-        return cls.objects.raw(query)
-
-    @classmethod
-    def filter_(cls, author=None, is_published=None):
-        if author and is_published:
-            query = f"SELECT * FROM blog_post WHERE author_id={author} and is_published={is_published}"
-        elif author:
-            query = f"SELECT * FROM blog_post WHERE author_id={author}"
-        else:
-            query = f"SELECT * FROM blog_post WHERE is_published={is_published}"
-        return cls.objects.raw(query)
-
-
+    
 class Comment(models.Model):
     author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
