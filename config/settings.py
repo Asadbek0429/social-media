8b73a218d9fc0d95e1cd593c2d1d6eac4018813d
Asadbek0429 <ravshanbekovasadbek0429@gmail.com>
2024-06-21 20:05:33 +0500

    init

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ex&6f&++$as@rnea37s2byb3qsbmj3j(!0fd3a11_vlnfqlo1a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,16 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('auth/', include('authentication.urls')),
+
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
